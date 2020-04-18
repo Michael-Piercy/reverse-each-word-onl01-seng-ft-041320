9c2392315a59d84a1229ea474724e2cf06ea6a06
Michael Piercy <michael.piercy@zoho.com>
2020-04-18 00:00:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,15 +7,3 @@ def reverse_each_word(flip)
     end
     new_string.join(" ")
 end
-
-
-
-def reverse_each_word(flip)
-    array = flip.split
-    new_sentence = []
-    array.collect do |twist|
-        twist.reverse!
-        new_sentence << twist
-    end
-    new_sentence.join(" ")
-end
